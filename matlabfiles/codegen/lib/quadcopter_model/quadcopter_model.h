dbff2506a67f1fd217c81c9b4419b3046815ca8d
solal.baudoin <solal.baudoin@student-cs.fr>
2025-11-05 17:15:31 +0100

    add matlab lib code

---
+++ matlabfiles/codegen/lib/quadcopter_model/quadcopter_model.h
@@ -0,0 +1,33 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * quadcopter_model.h
+ *
+ * Code generation for function 'quadcopter_model'
+ *
+ */
+
+#ifndef QUADCOPTER_MODEL_H
+#define QUADCOPTER_MODEL_H
+
+/* Include files */
+#include "rtwtypes.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Function Declarations */
+extern void quadcopter_model(const double x[12], const double u[4],
+                             double dxdt[12]);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+/* End of code generation (quadcopter_model.h) */
