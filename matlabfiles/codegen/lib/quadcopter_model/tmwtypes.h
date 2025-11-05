1bbad6d1c611326c564387a429dc73e5d3c0e2bd
Solal Baudoin <solal.baudoin@student-cs.fr>
2025-11-05 18:01:24 +0100

    add compiled shared object

---
+++ matlabfiles/codegen/lib/quadcopter_model/tmwtypes.h
@@ -0,0 +1,17 @@
+#ifndef TMWTYPES_H
+#define TMWTYPES_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef double real_T;
+typedef float real32_T;
+typedef int8_t int8_T;
+typedef uint8_t uint8_T;
+typedef int16_t int16_T;
+typedef uint16_t uint16_T;
+typedef int32_t int32_T;
+typedef uint32_t uint32_T;
+typedef bool boolean_T;
+
+#endif /* TMWTYPES_H */
